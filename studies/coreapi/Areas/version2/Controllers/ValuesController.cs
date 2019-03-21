a8aaa882ad16772aa631ad96833b32b1e8e82a05
unknown <rechvladimir@hotmail.com>
2019-03-21 11:34:31 -0300

    Adding project to test ASP.NET CORE Web API with classic .NET Framework 2.0 assemblies and how to use Areas in project.

---
+++ studies/coreapi/Areas/version2/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreapi.Areas.version2.Controllers
+{
+    [Route("v2/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return id.ToString();
+        }
+    }
+}
