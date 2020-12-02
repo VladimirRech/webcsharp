bb22cc4c3c098cfd0ac2c05b473ef440491331c9
Vladimir Rech <rechvladimir@hotmail.com>
2020-12-01 21:57:49 -0300

    Aplicação console dot net core no Ubuntu

---
+++ HelloWord/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World on Ubuntu!");
+            var lst = GetSampleList();
+            Console.WriteLine($"List size is {lst.Count}");
+            
+            foreach(var item in lst)
+                Console.WriteLine(item);
+                
+                
+            Console.Write("Informe um item para localizar: ");
+            var qry = Console.ReadLine();
+            
+            if (lst.FirstOrDefault(o => o == qry) != null)
+                Console.WriteLine($"{qry} foi encontrado!");
+            else
+                Console.WriteLine($"{qry} não foi encontrado");
+        }
+        
+        static List<string> GetSampleList() 
+        {
+            return new List<string>(
+                new string[] { "item 1", "item 2", "item 3" });
+        }
+    }
+}
