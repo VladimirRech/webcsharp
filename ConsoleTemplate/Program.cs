e0348adf7c29e7c9379173e5ab7a388b7d2255f5
Vladimir Rech <rechvladimir@hotmail.com>
2021-03-15 20:47:41 -0300

    Creating Dotnet Core Console template.

---
+++ ConsoleTemplate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ConsoleTemplate
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"\"The length of the args is {args.Length}");
+            Console.WriteLine("Following are the args passed:");
+
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Arg {i + 1} = {args[i]}");
+            }
+        }
+    }
+}
