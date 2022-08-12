e63eab13ddb55e95416461a1b3d27dad6d118ee5
Vladimir Rech <rechvladimir@hotmail.com>
2022-08-12 16:09:32 -0300

    Intital attempt to define context.

--- study-schedule/src/study-schedule-api/Program.cs
+++ study-schedule/src/study-schedule-api/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using EFModeling.EntityProperties.FluentAPI.Required;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+IConfiguration configuration = new ConfigurationManager();
 
 // Add services to the container.
 
@@ -6,6 +12,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(configuration.GetConnectionString("TasksConnString")));
 
 var app = builder.Build();
 
