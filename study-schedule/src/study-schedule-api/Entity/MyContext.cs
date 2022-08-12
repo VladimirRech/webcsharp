f0f7c476c49495d7ac24c1d283e0af4425145744
Vladimir Rech <rechvladimir@hotmail.com>
2022-08-12 16:59:33 -0300

    Adding some validations.

--- study-schedule/src/study-schedule-api/Entity/MyContext.cs
+++ study-schedule/src/study-schedule-api/Entity/MyContext.cs
@@ -1,18 +1,26 @@
 using Microsoft.EntityFrameworkCore;
-namespace EFModeling.EntityProperties.FluentAPI.Required;
+//namespace EFModeling.EntityProperties.FluentAPI.Required;
 using study_schedule_api.Classes;
+namespace study_schedule_api.DbContexts;
 
 public class MyContext : DbContext
 {
     public DbSet<StudyTask> StudyTask { get; set; }
 
-    public MyContext(DbContextOptions options) : base(options) 
-    {  
-        
+    public MyContext(DbContextOptions options) : base(options)
+    {
+
     }
-    protected override void OnModelCreating(ModelBuilder modelBuilder){
+
+    // Initial validations
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
         modelBuilder.Entity<StudyTask>()
             .Property(b => b.Id)
+            .IsRequired();
+
+        modelBuilder.Entity<StudyTask>()
+            .Property(b => b.Title)
             .IsRequired();
     }
 }
