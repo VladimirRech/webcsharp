7f012b90d5a0f3eb3e61daaa9bf245a563a5de19
Vladimir Rech <rechvladimir@hotmail.com>
2022-08-12 16:21:31 -0300

    Using dbcontect creation in another way.

--- study-schedule/src/study-schedule-api/Program.cs
+++ study-schedule/src/study-schedule-api/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using EFModeling.EntityProperties.FluentAPI.Required;
-using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-IConfiguration configuration = new ConfigurationManager();
+// IConfiguration configuration = new ConfigurationManager();
+
+// Reading String Connectiong
+string connString = builder.Configuration.GetConnectionString("TasksConnString");
+
+builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(connString));
 
 // Add services to the container.
 
@@ -12,7 +17,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(configuration.GetConnectionString("TasksConnString")));
+// builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(configuration.GetConnectionString("TasksConnString")));
 
 var app = builder.Build();
 
