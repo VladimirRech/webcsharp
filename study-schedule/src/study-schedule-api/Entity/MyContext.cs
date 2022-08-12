680f49f4ebba1efa8173d81547118fd8131b5446
Vladimir Rech <rechvladimir@hotmail.com>
2022-08-12 16:24:25 -0300

    Configuring context initialization

--- study-schedule/src/study-schedule-api/Entity/MyContext.cs
+++ study-schedule/src/study-schedule-api/Entity/MyContext.cs
@@ -6,6 +6,10 @@ public class MyContext : DbContext
 {
     public DbSet<StudyTask> StudyTask { get; set; }
 
+    public MyContext(DbContextOptions options) : base(options) 
+    {  
+        
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder){
         modelBuilder.Entity<StudyTask>()
             .Property(b => b.Id)
